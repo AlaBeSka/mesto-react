5ef8c8b1107697a970ba26f3201021ce29e83fdd
AlaBeSka <ehanbegov@yandex.ru>
2023-05-15 20:49:59 +0300

    убрал бесконечные запросы

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,12 @@ import React from "react";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 function Card({card, onCardClick, onCardLike, onCardDelete}) {
+
     const currentUser = React.useContext(CurrentUserContext);
-    const isOwn = currentUser && card.owner._id === currentUser._id;
-    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    const isOwn = card?.owner._id === currentUser._id;
+    const isLiked = card?.likes.some(i => i._id === currentUser._id);
     const cardLikeButtonClassName = `element__like-button ${isLiked && "element__like-button_active"}`;
+
 
     function handleClick() {
         onCardClick(card);
@@ -23,18 +25,18 @@ function Card({card, onCardClick, onCardLike, onCardDelete}) {
 return(
         <article className="element">
             <div className="element__photocard">
-                <img src={card.link} alt={card.name} className="element__image" 
+                <img src={card?.link} alt={card?.name} className="element__image" 
                      onClick={handleClick}/>
                 {isOwn && (<button name='popup-Card-delete' className="element__delete-button"
                                    onClick={handleDeleteCard}>
                 </button>)}
             </div>
             <div className="element__vote">
-                <h2 className="element__title">{card.name}</h2>
+                <h2 className="element__title">{card?.name}</h2>
                 <div className="element__like">
                     <button type="button" className={cardLikeButtonClassName} onClick={handleCardLike}>
                     </button>
-                    <p className="element__counter">{card.likes.length}</p>
+                    <p className="element__counter">{card?.likes.length}</p>
                 </div>
             </div>
         </article>
